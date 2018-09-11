12d19669a92c6544edf5d835ca318cebbcb6f000
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2018-09-11 10:42:27 -0500

    added donate button to header

--- sidebar.php
+++ sidebar.php
@@ -8,11 +8,13 @@
 	<div id="secondary" class="widget-area" role="complementary">
 		<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 		<div class="sidebar-primary">
+                  <div class="sidebar-donate">
+                          <img class="sidebar-donate-img" src="/wp-content/themes/apostrophe-child/images/wsa-donate-button.png" />
+                  </div>
                   <div class="sidebar-passport">
                           <div class="sidebar-apply-here">APPLY HERE&nbsp;</div>
-                          <img "sidebar-passport-img" src="/wp-content/themes/apostrophe-child/images/docpass.gif" />
+                          <img class="sidebar-passport-img" src="/wp-content/themes/apostrophe-child/images/docpass.gif" />
                   </div>
-                          <!--<img class="sidebar-passport" src="/wp-content/themes/apostrophe-child/images/docpass.gif" />-->
                   <div class="sidebar-after-passport">
 		    <?php dynamic_sidebar( 'sidebar-1' ); ?>
                   </div>
