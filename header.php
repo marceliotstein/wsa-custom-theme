526a476e0b301fc428592f2abc69d0f1c7b92072
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2018-09-11 11:13:30 -0500

    added social media icons to header

--- header.php
+++ header.php
@@ -20,6 +20,14 @@
 		<div id="page" class="hfeed site">
 
 			<header id="masthead" class="site-header" role="banner">
+                                <div class="social-icons">
+                                  <a href="#" alt="Follow us on Facebook" class="fa fa-facebook"></a>
+                                  <a href="#" alt="Follow us on Instagram" class="fa fa-instagram"></a>
+                                  <a href="#" alt="Follow us on Twitter" class="fa fa-twitter"></a>
+                                  <a href="#" alt="Follow us on YouTube" class="fa fa-youtube"></a>
+                                  <a href="#" alt="Connect with us on LinkedIn" class="fa fa-linkedin"></a>
+                                  <a href="#" alt="Subscribe via Yahoo" class="fa fa-yahoo"></a>
+                                </div>
 				<div style="cursor: pointer;" onclick="window.location='/';" class="site-branding-wsa-custom">
 					<?php
 					if ( function_exists( 'jetpack_the_site_logo' ) ) :
