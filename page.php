57384a77f1dc680552130485d56a9663e18dac78
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2019-02-25 09:56:09 -0600

    narrow viewport for front page and styles for interior pages

--- page.php
+++ page.php
@@ -11,44 +11,44 @@
  */
 
 get_header(); ?>
+  <section id="primary" class="content-area">
+	<nav id="site-navigation" class="main-navigation" role="navigation">
+		<a class="menu-toggle"><?php esc_html_e( 'Menu', 'apostrophe' ); ?></a>
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'apostrophe' ); ?></a>
 
-	<section id="primary" class="content-area">
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<a class="menu-toggle"><?php esc_html_e( 'Menu', 'apostrophe' ); ?></a>
-			<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'apostrophe' ); ?></a>
+		<?php wp_nav_menu( array(
+			'theme_location' => 'primary',
+			'menu_class'     => 'apostrophe-navigation',
+		) ); ?>
 
-			<?php wp_nav_menu( array(
-				'theme_location' => 'primary',
-				'menu_class'     => 'apostrophe-navigation',
-			) ); ?>
+		<?php wp_nav_menu( array(
+			'theme_location' => 'social',
+			'menu_class'     => 'apostrophe-social',
+			'link_before'    => '<span>',
+			'link_after'     => '</span>',
+			'fallback_cb'    => '',
+			'depth'          => 1,
+		) ); ?>
 
-			<?php wp_nav_menu( array(
-				'theme_location' => 'social',
-				'menu_class'     => 'apostrophe-social',
-				'link_before'    => '<span>',
-				'link_after'     => '</span>',
-				'fallback_cb'    => '',
-				'depth'          => 1,
-			) ); ?>
+ 	</nav><!-- #site-navigation -->
+	<main id="main" class="site-main" role="main">
 
-		</nav><!-- #site-navigation -->
-		<main id="main" class="site-main" role="main">
+		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content', 'page' ); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+			<?php
+			// If comments are open or we have at least one comment, load up the comment template
+			if ( comments_open() || '0' !== get_comments_number() ) :
+				comments_template();
+				endif;
+			?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' !== get_comments_number() ) :
-					comments_template();
-					endif;
-				?>
+		<?php endwhile; // end of the loop. ?>
 
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
+                <hr />
+                <?php get_sidebar(); ?>
+                <hr />
+	</main><!-- #main -->
+  </section><!-- #primary -->
 <?php get_footer(); ?>
