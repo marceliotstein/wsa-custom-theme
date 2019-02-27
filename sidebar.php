eb1d72536d09992a49244e083d497fc44adf43d9
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2019-02-27 10:23:49 -0600

    cleanup sidebar position and background colors

--- sidebar.php
+++ sidebar.php
@@ -5,25 +5,18 @@
  * The Sidebar containing the main widget areas.
  */
 ?>
-	<div id="secondary" class="widget-area" role="complementary">
-		<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-		<div class="sidebar-primary">
-                  <div class="sidebar-passport">
-                          <img class="sidebar-passport-img" src="/wp-content/themes/apostrophe-child/images/docpass.gif" />
-                          <div class="sidebar-apply-here">APPLY&nbsp;HERE&nbsp;</div>
-                  </div>
-                  <div class="sidebar-after-passport">
-		    <?php dynamic_sidebar( 'sidebar-1' ); ?>
-                  </div>
-		</div>
-		<?php endif; ?>
+<div id="secondary" class="widget-area" role="complementary">
+  <div class="sidebar-primary sidebar-gray">
+    <div class="sidebar-passport">
+      <img class="sidebar-passport-img" src="/wp-content/themes/apostrophe-child/images/docpass.gif" />
+      <div class="sidebar-apply-here">APPLY&nbsp;HERE&nbsp;</div>
+    </div>
+    <div class="sidebar-after-passport sidebar-gray">
+      <?php dynamic_sidebar( 'sidebar-1' ); ?>
 
-                <?php // sidebar-secondary is moved to front page ?>
-
-		<?php if ( is_active_sidebar( 'sidebar-3' ) ) : ?>
-		<div class="sidebar-tertiary">
-			<?php dynamic_sidebar( 'sidebar-3' ); ?>
-		</div>
-		<?php endif; ?>
-
-	</div><!-- #secondary -->
+      <div class="sidebar-yellow">
+        <?php dynamic_sidebar( 'sidebar-3' ); ?>
+      </div>
+    </div>
+  </div>
+</div><!-- #secondary -->
